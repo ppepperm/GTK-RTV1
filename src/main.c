62b45542c203327b1818aba8f2a980e65cccfa53
pepperm <arseniy@medinskiy.net>
2020-06-30 15:34:58 +0300

    aaaa

--- src/main.c
+++ src/main.c
@@ -12,15 +12,24 @@
 
 #include "../includes/rt.h"
 
+void draw_to_texture(t_scene scene, SDL_Texture *win_tex)
+{
+	unsigned char	*win_buff;
+	void			*tmp;
+	int 			pitch;
+
+	SDL_LockTexture(win_tex, NULL, &tmp, &pitch);
+	win_buff = (unsigned char *)tmp;
+	draw_scene(scene, win_buff, pitch);
+	SDL_UnlockTexture(win_tex);
+}
+
 int main( void)
 {
 	SDL_Window* window = NULL;
 	SDL_Event	event;
-	SDL_Renderer* renderer;
+	SDL_Renderer *renderer;
 	SDL_Texture *win_tex;
-	unsigned char	*win_buff;
-	void			*tmp;
-	int 			pitch;
 	t_scene			scene;
 
 	SDL_Init( SDL_INIT_VIDEO );
@@ -28,15 +37,80 @@ int main( void)
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
 	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
 	win_tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, W_W, W_H);
 	scene = read_scene("scene");
-	SDL_LockTexture(win_tex, NULL, &tmp, &pitch);
-	win_buff = (unsigned char *)tmp;
-	draw_scene(scene, win_buff, pitch);
-	SDL_UnlockTexture(win_tex);
+	draw_to_texture(scene, win_tex);
 	while (!(SDL_PollEvent(&event) && event.type == SDL_QUIT))
 	{
+		if (event.type == SDL_KEYDOWN)
+		{
+			if (event.key.keysym.scancode == SDL_SCANCODE_W)
+			{
+				scene.camera.pos = lin_comb(scene.camera.pos, 1, scene.camera.z_dir, 0.5);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_S)
+			{
+				scene.camera.pos = lin_comb(scene.camera.pos, 1, scene.camera.z_dir, -0.5);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_A)
+			{
+				scene.camera.pos = lin_comb(scene.camera.pos, 1, scene.camera.x_dir, -0.5);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_D)
+			{
+				scene.camera.pos = lin_comb(scene.camera.pos, 1, scene.camera.x_dir, 0.5);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_LCTRL)
+			{
+				scene.camera.pos = lin_comb(scene.camera.pos, 1, scene.camera.y_dir, -0.5);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_LSHIFT)
+			{
+				scene.camera.pos = lin_comb(scene.camera.pos, 1, scene.camera.y_dir, 0.5);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+			{
+				scene.camera.x_dir = rotate(scene.camera.x_dir, scene.camera.y_dir, -1);
+				scene.camera.z_dir = rotate(scene.camera.z_dir, scene.camera.y_dir, -1);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+			{
+				scene.camera.x_dir = rotate(scene.camera.x_dir, scene.camera.y_dir, 1);
+				scene.camera.z_dir = rotate(scene.camera.z_dir, scene.camera.y_dir, 1);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_UP)
+			{
+				scene.camera.y_dir = rotate(scene.camera.y_dir, scene.camera.x_dir, -1);
+				scene.camera.z_dir = rotate(scene.camera.z_dir, scene.camera.x_dir, -1);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_DOWN)
+			{
+				scene.camera.y_dir = rotate(scene.camera.y_dir, scene.camera.x_dir, 1);
+				scene.camera.z_dir = rotate(scene.camera.z_dir, scene.camera.x_dir, 1);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_Q)
+			{
+				scene.camera.x_dir = rotate(scene.camera.x_dir, scene.camera.z_dir, 1);
+				scene.camera.y_dir = rotate(scene.camera.y_dir, scene.camera.z_dir, 1);
+				draw_to_texture(scene, win_tex);
+			}
+			else if (event.key.keysym.scancode == SDL_SCANCODE_E)
+			{
+				scene.camera.x_dir = rotate(scene.camera.x_dir, scene.camera.z_dir, -1);
+				scene.camera.y_dir = rotate(scene.camera.y_dir, scene.camera.z_dir, -1);
+				draw_to_texture(scene, win_tex);
+			}
+		}
 		SDL_RenderCopy(renderer, win_tex, NULL, NULL);
 		SDL_RenderPresent(renderer);
 	}
