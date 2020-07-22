b17ff75ae793e7161d2aa274e7ff7d881929f0f6
Peppermint Peppermint <ppepperm@oa-g5.msk.21-school.ru>
2020-07-22 15:52:16 +0300

    make

---
+++ SDL2-2.0.12/build/.libs/SDL_rwopsbundlesupport.d
@@ -0,0 +1,8 @@
+build/SDL_rwopsbundlesupport.lo: \
+  /Users/ppepperm/RTV1/SDL2-2.0.12/src/file/cocoa/SDL_rwopsbundlesupport.m \
+  /Users/ppepperm/RTV1/SDL2-2.0.12/src/file/cocoa/../../SDL_internal.h \
+  /Users/ppepperm/RTV1/SDL2-2.0.12/src/file/cocoa/../../dynapi/SDL_dynapi.h \
+  /Users/ppepperm/RTV1/SDL2-2.0.12/src/file/cocoa/../../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h \
+  /Users/ppepperm/RTV1/SDL2-2.0.12/src/file/cocoa/SDL_rwopsbundlesupport.h
