6bf29225884d15dc662617f25ba8f61664c0e7f4
Peppermint Peppermint <ppepperm@mi-o5.msk.21-school.ru>
2020-07-24 18:08:11 +0300

    aaa

--- src/intersections.c
+++ src/intersections.c
@@ -25,7 +25,8 @@ t_p2			intersect_sphere(t_ray ray, t_object object)
 	k.z = sc_mult(ray.pos, ray.pos) - sphere->r * sphere->r;
 	d = k.y * k.y - 4 * k.x * k.z;
 	if (d >= 0)
-		return (init_p2((-k.y - sqrt(d)) / (2 * k.x), (-k.y + sqrt(d)) / (2 * k.x)));
+		return (init_p2((-k.y - sqrt(d))\
+		/ (2 * k.x), (-k.y + sqrt(d)) / (2 * k.x)));
 	else
 		return (init_p2(-1, -1));
 }
@@ -55,12 +56,16 @@ t_p2			intersect_cone(t_ray ray, t_object object)
 	cone = (t_cone*)object.data;
 	ray = ray_transform(ray, object.i_t, object.pos);
 	c = cone->r / cone->c;
-	k.x = (ray.dir.x * ray.dir.x + ray.dir.z * ray.dir.z - ray.dir.y * ray.dir.y * c * c);
-	k.y = 2 * (ray.dir.x * ray.pos.x + ray.dir.z * ray.pos.z - ray.dir.y * ray.pos.y * c * c);
-	k.z = (ray.pos.x * ray.pos.x + ray.pos.z * ray.pos.z - ray.pos.y * ray.pos.y * c * c);
+	k.x = (ray.dir.x * ray.dir.x\
+	+ ray.dir.z * ray.dir.z - ray.dir.y * ray.dir.y * c * c);
+	k.y = 2 * (ray.dir.x * ray.pos.x\
+	+ ray.dir.z * ray.pos.z - ray.dir.y * ray.pos.y * c * c);
+	k.z = (ray.pos.x * ray.pos.x\
+	+ ray.pos.z * ray.pos.z - ray.pos.y * ray.pos.y * c * c);
 	d = k.y * k.y - 4 * k.x * k.z;
 	if (d >= 0)
-		return (init_p2((-k.y - sqrt(d)) / (2 * k.x), (-k.y + sqrt(d)) / (2 * k.x)));
+		return (init_p2((-k.y - sqrt(d))\
+		/ (2 * k.x), (-k.y + sqrt(d)) / (2 * k.x)));
 	else
 		return (init_p2(-1, -1));
 }
