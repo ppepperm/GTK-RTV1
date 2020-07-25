e07fed049b66e3a513eaaaa6995e007eb38cc87e
Peppermint Peppermint <ppepperm@oa-g1.msk.21-school.ru>
2020-07-25 16:57:24 +0300

    asdadasd

--- SDL2-2.0.12/build/.libs/SDL_log.d
+++
@@ -1,7 +0,0 @@
-build/SDL_log.lo: /Users/ppepperm/RTV1/SDL2-2.0.12/src/SDL_log.c \
-  /Users/ppepperm/RTV1/SDL2-2.0.12/src/./SDL_internal.h \
-  /Users/ppepperm/RTV1/SDL2-2.0.12/src/dynapi/SDL_dynapi.h \
-  /Users/ppepperm/RTV1/SDL2-2.0.12/src/dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_error.h include/SDL_stdinc.h \
-  include/SDL_log.h
