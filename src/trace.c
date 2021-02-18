bd1d00462b1628bf2a0a132db59431c269e62ab7
Мединский Арсений Владимирович <AVMedinskiy@sberbank.ru>
2021-02-18 14:24:39 +0300

    aaa

--- src/trace.c
+++ src/trace.c
@@ -39,7 +39,7 @@ double			get_intersection(t_ray ray,
 	t_p2	roots;
 	t_p2	new_roots;
 
-	*colour = init_rgb(0, 0, 0, 255);
+	*colour = init_rgb(250, 250, 250, 255);
 	*current = NULL;
 	roots = init_p2(3000000, 3000000);
 	while (scene.objects)
@@ -80,8 +80,28 @@ t_rgb			trace_ray(t_ray ray, t_scene scene, int depth)
 	root = get_intersection(ray, scene, &current, &colour);
 	if (!current)
 		return (colour);
-	if (current && current == scene.chosen)
-		return (init_rgb(255, 255, 0, 255));
+    if (current && current == scene.chosen)
+        colour = init_rgb(255, 255, 0, 255);
+    if (current->type == T_SPHERE) {
+        t_p3 inter;
+        double alpha;
+        double beta;
+        inter = lin_comb(ray.pos, 1, ray.dir, root);
+        inter = transform_pos(inter, current->i_t, current->pos);
+        normalize(&inter);
+        alpha = acos(inter.x);
+        beta = atan2(inter.y, inter.z);
+        if (beta < 0)
+            beta += 3.14;
+        alpha = alpha * 10 / 3.14;
+        beta = beta * 10 / 3.14;
+        if ((int) alpha % 2 == (int) beta % 2) {
+            colour = init_rgb(250, 0, 0, 255);
+        } else {
+            colour = init_rgb(0, 0, 255, 255);
+        }
+    }
+
 	colour = colour_mult(colour,\
 	current->light_funk(scene.lights, *current, ray, root));
 
@@ -92,9 +112,9 @@ t_rgb			trace_ray(t_ray ray, t_scene scene, int depth)
 	reflected_colour = trace_ray(reflected, scene, depth + 1);
 
 	t_rgb ret;
-	ret.r = (colour.r + reflected_colour.r)/2;
-	ret.g = (colour.g + reflected_colour.g)/2;
-	ret.b = (colour.b + reflected_colour.b)/2;
+	ret.r = colour.r*0.6 + reflected_colour.r*0.4;
+	ret.g = colour.g*0.6 + reflected_colour.g*0.4;
+	ret.b = colour.b*0.6 + reflected_colour.b*0.4;
 	ret.a = 255;
 
 	return (ret);
@@ -107,7 +127,7 @@ int				draw_scene(t_scene scene, unsigned char *win_buff, int pitch)
 	pthread_t		*threads;
 	pthread_attr_t	attr;
 
-	init_threads(&data, &threads, &attrc);
+	init_threads(&data, &threads, &attr);
 	if (!data || !threads)
 		return (0);
 	count.x = 0;
